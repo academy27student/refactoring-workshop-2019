dbf2b244c7919fed9b88dba4f8f413fc3315498f
a21stude <a21stude@a21stude-HP-ProBook-640-G2>
2019-04-18 19:59:12 +0200

    Zadanie 3

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -65,6 +65,10 @@ Controller::Controller(IPort& p_displayPort, IPort& p_foodPort, IPort& p_scorePo
 
 void Controller::handleTimePassed(const TimeoutInd&)
 {
+    if(isPaused)
+    {
+        return;
+    }
     Segment newHead = getNewHead();
 
     if(doesCollideWithSnake(newHead))
@@ -99,6 +103,10 @@ void Controller::handleTimePassed(const TimeoutInd&)
 
 void Controller::handleDirectionChange(const DirectionInd& directionInd)
 {
+    if(isPaused)
+    {
+        return;
+    }
     auto direction = directionInd.direction;
 
     if ((m_currentDirection & 0b01) != (direction & 0b01)) {
@@ -148,6 +156,11 @@ void Controller::handleNewFood(const FoodResp& requestedFood)
     }
 
     m_foodPosition = std::make_pair(requestedFood.x, requestedFood.y);
+}
+
+void Controller::handlePause()
+{
+    isPaused = !isPaused;
 }
 
 bool Controller::doesCollideWithSnake(const Controller::Segment &newSegment) const
@@ -217,10 +230,12 @@ void Controller::receive(std::unique_ptr<Event> e)
 {
     switch(e->getMessageId())
     {
+
         case TimeoutInd::MESSAGE_ID: return handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
         case DirectionInd::MESSAGE_ID: return handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
         case FoodInd::MESSAGE_ID: return handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
         case FoodResp::MESSAGE_ID: return handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
+        case PauseInd::MESSAGE_ID: return handlePause();
         default: throw UnexpectedEventException();
     };
 }
